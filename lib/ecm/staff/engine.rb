eea645e563e1e7f69791182a3c2e84690b156470
Roberto Vasquez Angel <robotex@robotex.de>
2015-07-05 23:15:20 +0200

    Bugfix: Added ecm_staff_active_admin.css to the asset pipeline.

--- lib/ecm/staff/engine.rb
+++ lib/ecm/staff/engine.rb
@@ -6,6 +6,10 @@ module Ecm
           config.load_paths << File.join(self.root, 'lib/ecm/staff/active_admin')
 
           config.register_stylesheet 'ecm_staff_active_admin.css'
+
+          Rails.application.config.assets.precompile += %w(
+            ecm_staff_active_admin.css
+          )
         end
       end # initializer :ecm_staff_engine do
     end # class Engine < ::Rails::Engine
--- lib/ecm/staff/version.rb
+++ lib/ecm/staff/version.rb
@@ -1,6 +1,6 @@
 module Ecm
   module Staff
-    VERSION = "0.0.1.pre"
+    VERSION = "0.0.2.pre"
   end # module Staff
 end # module Ecm
 
